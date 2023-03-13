af01af7999144fc568cc5f7f961ff11113826092
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-13 17:22:11 +0100

    Setup Eslint + Prettier

--- src/pages/api/coffees/index.ts
+++ src/pages/api/coffees/index.ts
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    res.status(200).json({ coffee: 'Coffee' })
+  res.status(200).json({ coffee: 'Coffee' });
 }
