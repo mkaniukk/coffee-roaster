d29a4708a91fd9c79f59c43d778c7c29999141a7
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-13 16:47:28 +0100

    Rename function

--- src/pages/coffees/[id].tsx
+++ src/pages/coffees/[id].tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 
-export default function Coffees() {
+export default function Coffee() {
   return (
     <>
       <Head>
