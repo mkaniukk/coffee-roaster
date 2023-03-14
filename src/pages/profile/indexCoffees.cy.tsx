e8321664fc083140521447933ef404be50768f79
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-14 23:33:25 +0100

    Remove Cypress component tests

--- src/pages/profile/indexCoffees.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Coffees from './index'
-
-describe('<Coffees />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Coffees />)
-  })
-})
