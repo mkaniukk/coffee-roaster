ade3e5745311ccbfb1f6d2ee3859b7c7f862837d
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-15 15:50:46 +0100

    Added subpage testing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,4 +2,16 @@ describe('template spec', () => {
     it('passes', () => {
         cy.visit('/');
     });
+    it('passes', () => {
+        cy.visit('/coffees');
+    });
+    it('passes', () => {
+        cy.visit('/login');
+    });
+    it('passes', () => {
+        cy.visit('/map');
+    });
+    it('passes', () => {
+        cy.visit('/profile');
+    });
 });
