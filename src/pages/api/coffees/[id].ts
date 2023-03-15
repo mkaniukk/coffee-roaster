82ebf75adcc183642a6e325316ff0337ae179994
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-15 15:50:34 +0100

    Added API coffees/[id]

--- src/pages/api/coffees/[id].ts
+++ src/pages/api/coffees/[id].ts
@@ -1,7 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import clientPromise from '../../../../lib/mongodb';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
     const { id } = req.query;
-    res.status(200).json({ id: id });
+    try {
+        const client = await clientPromise;
+        const db = client.db('coffee-roaster');
+
+        const coffee = await db
+            .collection('coffees')
+            .find({ Id: id })
+            .toArray();
+
+        if (!Array.isArray(coffee) || !coffee.length)
+            res.status(404).send('Error 404: No coffee with a given Id.');
+        res.status(200).json(coffee);
+    } catch (e) {
+        console.error(e);
+    }
 }
