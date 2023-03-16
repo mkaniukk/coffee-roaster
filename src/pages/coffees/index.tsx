46f1ff4e92d18194e6980060af741bb2e4c6d680
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-16 18:22:39 +0100

    Added coffees list

--- src/pages/coffees/index.tsx
+++ src/pages/coffees/index.tsx
@@ -1,7 +1,13 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
+import { Coffee } from '../../models/Coffee/CoffeeModel';
+import Link from 'next/link';
 
-export default function CoffeesPage() {
+type CoffeeParams = {
+    coffees: Array<Coffee>;
+};
+
+export default function CoffeesPage({ coffees }: CoffeeParams) {
     return (
         <>
             <Head>
@@ -18,7 +24,35 @@ export default function CoffeesPage() {
             </Head>
             <main className={styles.main}>
                 <div className={styles.header}>Coffees Page</div>
+                <ul>
+                    {coffees.map((coffee) => {
+                        return (
+                            <Link
+                                key={coffee.Id}
+                                className={styles.card}
+                                href={`/coffees/${+coffee.Id - 1}`}
+                            >
+                                <button className={styles.header}>
+                                    {coffee.Id}
+                                </button>
+                            </Link>
+                        );
+                    })}
+                </ul>
             </main>
         </>
     );
+}
+
+export async function getStaticProps() {
+    const coffees = await fetch(`http://127.0.0.1:3000/api/coffees`).then(
+        (data) => {
+            return data.json();
+        },
+    );
+
+    return {
+        // Passed to the page component as props
+        props: { coffees },
+    };
 }
