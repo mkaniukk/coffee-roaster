3926ff9b02ae5451c847f02ed07d5b196fb582b9
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-17 22:05:47 +0100

    Updated Coffee Model

--- src/models/Coffee/CoffeeModel.ts
+++ src/models/Coffee/CoffeeModel.ts
@@ -1,5 +1,26 @@
+import { ObjectId } from 'mongodb';
+
 export type Coffee = {
-    _id: number;
+    _id: ObjectId;
     Id: number;
-    Owner: string[];
+    Acidity: number;
+    Aftertaste: number;
+    Altitude: string;
+    Aroma: number;
+    Balance: number;
+    Body: number;
+    Color: string | undefined;
+    Company: string | undefined;
+    Country: { Of: { Origin: string } };
+    Cupper: { Points: number };
+    Flavor: number;
+    Harvest: { Year: string };
+    Moisture: number;
+    Owner: Array<string> | string | null;
+    Producer: string | undefined;
+    Region: string | undefined;
+    Sweetness: number;
+    Total: { Cup: { Points: number } };
+    Uniformity: number;
+    Variety: string | undefined;
 };
--- src/pages/api/graphql/index.ts
+++ src/pages/api/graphql/index.ts
@@ -4,6 +4,7 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
+
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
