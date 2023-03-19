69d8ec4f71d25d3510671f304ae65399422a23c7
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-19 14:13:26 +0100

    Added place text search

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    env: {
+        MONGODB_URI: process.env.MONGODB_URI,
+        GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+        MODE: process.env.NODE_ENV,
+        API_URI: process.env.API_URI,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/ components/MapComponent.tsx
+++ src/ components/MapComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import { Marker } from '@react-google-maps/api';
 
@@ -14,7 +14,26 @@ const center = {
     lng: -38.523,
 };
 
+const parameters = { query: 'Museum of Contemporary Art Australia' };
+
 const MapComponent = () => {
+    const [place, setPlace] = useState(center);
+
+    useEffect(() => {
+        const getPlaceData = async () => {
+            const place = await fetch(
+                `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${googleMapsApiKey}&query=${parameters.query}`,
+            ).then((data) => {
+                console.log(data);
+                return data.json();
+            });
+            console.log(place.results[0].geometry.location);
+            const newPlace = place.results[0].geometry.location;
+            setPlace({ ...newPlace });
+        };
+        getPlaceData();
+    }, []);
+
     return (
         <LoadScript googleMapsApiKey={googleMapsApiKey}>
             <GoogleMap
