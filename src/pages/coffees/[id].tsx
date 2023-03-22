8ecbfc0e8d507cafa71daaedb5699a0e886048f8
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-22 23:45:25 +0100

    Changed styles

--- src/pages/coffees/[id].tsx
+++ src/pages/coffees/[id].tsx
@@ -35,7 +35,7 @@ export default function CoffeePage({ coffee }: CoffeeParams) {
             </Head>
             <main className={styles.main}>
                 <NavBar />
-                <a className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <a className="block h-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                         Coffee {coffee.Id} Page{' '}
                     </h5>
