2cc1e9c8988a7feab85850c2791b9ac3f17ef0b1
mkaniuk <m.kaniuk@stud.elka.pw.edu.pl>
2023-03-24 00:03:37 +0100

    Added StaticProps to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,14 @@ import Link from 'next/link';
 import styles from '@/styles/Home.module.css';
 import MapComponent from '@/ components/MapComponent';
 import NavBar from '@/ components/NavBarComponent';
+import CoffeesMapComponent from '@/ components/CoffeesMapComponent';
+import { Coffee } from '@/models/Coffee/CoffeeModel';
 
-export default function Home() {
+type CoffeesMapProps = {
+    locations: string;
+};
+
+export default function Home({ locations }: CoffeesMapProps) {
     return (
         <>
             <Head>
@@ -13,8 +19,35 @@ export default function Home() {
             </Head>
             <main className="grid-rows-2 h-full">
                 <NavBar />
-                <MapComponent />
+                <CoffeesMapComponent locations={[locations]} />
             </main>
         </>
     );
+}
+
+export async function getStaticProps() {
+    const coffees = await fetch(`http://127.0.0.1:3000/api/coffees`).then(
+        (data) => {
+            return data.json();
+        },
+    );
+
+    const locations = coffees
+        .map((coffee: Coffee) => {
+            return (
+                coffee?.Farm?.Name +
+                ' ' +
+                coffee?.Region +
+                ' ' +
+                coffee?.Country?.of?.Origin
+            );
+        })
+        .slice(0, 20);
+
+    console.log(locations);
+
+    return {
+        // Passed to the page component as props
+        props: { locations },
+    };
 }
